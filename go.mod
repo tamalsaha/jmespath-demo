34c0fc77eed98014014683514713892e16c324f6
Tamal Saha <tamal@appscode.com>
2024-09-08 09:44:04 -0700

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tamalsaha/jmespath-demo
 
 go 1.23.1
+
+require github.com/jmespath/go-jmespath v0.4.0
--- main.go
+++ main.go
@@ -1,3 +1,64 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jmespath/go-jmespath"
+)
+
+func main() {
+	var jsondata = []byte(`{
+  "apiVersion": "rbac.authorization.k8s.io/v1",
+  "kind": "Role",
+  "metadata": {
+    "namespace": "default",
+    "name": "configmap-updater"
+  },
+  "rules": [
+    {
+      "apiGroups": [
+        ""
+      ],
+      "resources": [
+        "configmaps"
+      ],
+      "resourceNames": [
+        "my-configmap"
+      ],
+      "verbs": [
+        "update",
+        "get"
+      ]
+    },
+    {
+      "apiGroups": [
+        ""
+      ],
+      "resources": [
+        "secrets"
+      ],
+      "resourceNames": [
+        "my-secret"
+      ],
+      "verbs": [
+        "update",
+        "get"
+      ]
+    }
+  ]
+}`) // your data
+	var data interface{}
+	err := json.Unmarshal(jsondata, &data)
+	if err != nil {
+		panic(err)
+	}
+	result, err := jmespath.Search("rules[?contains(resources, 'secrets') == `true`].resourceNames[]", data)
+	if err != nil {
+		panic(err)
+	}
+	jb, err := json.MarshalIndent(result, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(jb))
+}
